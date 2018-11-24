948a05051c73cc5efffb04bbb5a305eca5103a6e
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-24 07:45:59 -0800

    added get_next_line.c get_next_line.h get_next_line.h.gch for getnxtl

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,26 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 21:04:41 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/23 16:15:06 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/24 06:58:25 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 42
+# define FD_LIMIT 4864
 
 # include "libft/libft.h"
-#include <sys/types.h>
-# include <unistd.h>
+# include <sys/types.h>
 # include <fcntl.h>
-//# include <sys/types.h>
-//# include <sys/stat.h>
 
-int		get_next_line(const int fd, char **line);
+typedef struct	s_file
+{
+	int		offset;
+	char	*str;
+	int		len;
+}				t_file;
+
+int				get_next_line(const int fd, char **line);
 
 #endif
