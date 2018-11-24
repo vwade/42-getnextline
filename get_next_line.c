159b4ee18ed11904edbef08bb7c6943fee9c126a
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-24 07:46:10 -0800

    added get_next_line.c get_next_line.h get_next_line.h.gch for getnxtl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 21:04:41 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/24 07:45:41 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/24 07:46:07 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,23 @@ static int
 	return (i);
 }
 
+static int
+	readline(t_file **file, char **line, int fd)
+{
+	;
+}
+
+/*
+**	COMMENTS MAY GO HERE
+*/
+
 int
 	get_next_line(const int fd, char **line)
 {
-	int			i;
-	static int	bytes;
-	static int	pfd;
-	char		*buf;
-	char		*tmp;
+	static t_file	*file[FD_LIMIT];
+	int				ret;
 
-	bytes *= fd == pfd;
-	pfd = fd;
-	i = 0;
-	buf = (char *)ft_memalloc(BUFF_SIZE);
-	while (0 < (bytes = (int)read(fd, (void *)buf, (size_t)BUFF_SIZE)))
-	{
-		++i;
-		tmp = ft_strnew(BUFF_SIZE * i + 1);
-		if (i > 1)
-			tmp = ft_strcat(tmp, line[0]);
-		free (line[0]);
-		ft_strncat(tmp, buf, modify(buf));
-		line[0] = ft_strdup(tmp);
-		free(tmp);
-	}
-	free(buf);
-	if (bytes == -1)
+	if (fd < 0 || fd > FD_LIMIT - 1 || !line)
 		return (-1);
-	return (1);
+	
 }
