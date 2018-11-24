21176b3423dbe30e6e880626bcf5b8bc142a549d
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-23 19:08:03 -0800

    added get_next_line.c get_next_line.h get_next_line.h.gch for getnxtl

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,54 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 21:04:41 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/18 21:17:57 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/23 19:07:46 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		get_next_line(const int fd, char **line)
+#include "get_next_line.h"
+
+static int
+	modify(char *b)
 {
-    ;
+	size_t	i;
+	size_t	n;
+
+	i = 0;
+	n = BUFF_SIZE;
+	while (n--)
+		if ((b[i] *= !(b[i] == '\n')))
+			break ;
+		else
+			++i;
+	return (i);
+}
+
+int
+	get_next_line(const int fd, char **line)
+{
+	int			i;
+	static int	bytes;
+	static int	pfd;
+	char		*buf;
+	char		*tmp;
+
+	bytes *= fd == pfd;
+	pfd = fd;
+	i = 0;
+	buf = (char *)ft_memalloc(BUFF_SIZE);
+	while (0 < (bytes = (int)read(fd, (void *)buf, (size_t)BUFF_SIZE)))
+	{
+		++i;
+		tmp = ft_strnew(BUFF_SIZE * i + 1);
+		if (i > 1)
+			tmp = ft_strcat(tmp, line[0]);
+		free (line[0]);
+		ft_strncat(tmp, buf, modify(buf));
+		line[0] = ft_strdup(tmp);
+		free(tmp);
+	}
+	free(buf);
+	if (bytes == -1)
+		return (-1);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,20 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 21:04:41 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/18 21:17:49 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/23 16:15:06 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 42
+
+# include "libft/libft.h"
+#include <sys/types.h>
+# include <unistd.h>
+# include <fcntl.h>
+//# include <sys/types.h>
+//# include <sys/stat.h>
 
 int		get_next_line(const int fd, char **line);
 
