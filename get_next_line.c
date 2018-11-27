771db585450239c9e3dafe13de449f18a19a6959
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-26 18:19:14 -0800

    added get_next_line.c get_next_line.h get_next_line.h.gch for getnxtl

--- get_next_line.c
+++ get_next_line.c
@@ -6,52 +6,36 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 21:04:41 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/25 10:49:18 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/26 18:19:03 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 static int
-	modify(char *b)
-{
-	size_t	i;
-	size_t	n;
-
-	i = 0;
-	n = BUFF_SIZE;
-	while (n--)
-		if ((b[i] *= !(b[i] == '\n')))
-			break ;
-		else
-			++i;
-	return (i);
-}
-
-static int
 	readline(t_file *file, int fd, char **line)
 {
+	int		nb;
 	char	buf[BUFF_SIZE + 1];
 	char	*tmp;
 
-	if ((file->offset = read(fd, buf, 0)) == -1)
-		return (-1);
-	while ((file->offset = read(fd, buf, BUFF_SIZE)) > )
+	if (!file->str)
+		file->str = ft_strnew(0);
+	while ((nb = read(fd, buf, BUFF_SIZE)) > 0)
 	{
+		buf[nb] = 0;
 		tmp = file->str;
 		file->str = ft_strjoin(tmp, buf);
 		file->len = ft_strlen(file->str);
 		free(tmp);
-		if ((tmp = ft_strchr(file->str, '\n')))
-			if ((file->offset++))
-				break ;
 	}
-	free(buf);
-	line[0] = ft_strsub(file->str, tmp - file->str, file->len);
-	line[0][file->len] = 0;
-	if (file->offset)
-		return (1);
-	return (0);
+	if ((tmp = ft_strchr(&file->str[file->ndx], '\n')))
+		file->len = tmp - &file->str[file->ndx];
+	else
+		file->len = ft_strlen(&file->str[file->ndx]);
+	line[0] = ft_strsub(file->str, file->ndx, file->len);
+	file->ndx = file->len + 1;
+	return (!!(file->str[file->ndx]));
 }
 
 /*
@@ -61,11 +45,9 @@ static int
 int
 	get_next_line(const int fd, char **line)
 {
-	static t_file	*file[FD_LIMIT];
-	int				ret;
+	static t_file	file[FD_LIMIT];
 
 	if (fd < 0 || fd > FD_LIMIT - 1 || !line)
 		return (-1);
-	ret = readline(file[fd], fd, line);
-	return (ret);
+	return (readline(&file[fd], fd, line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 21:04:41 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/24 11:24:37 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/26 17:57:54 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define FD_LIMIT 4864
+# define FD_LIMIT 8192
 # define BUFF_SIZE 42
 
 # include "libft/libft.h"
@@ -22,7 +22,7 @@
 
 typedef struct	s_file
 {
-	int		offset;
+	int		ndx;
 	char	*str;
 	int		len;
 }				t_file;
